8c8714348e2338121024ed4df6701038cc6e47c6
thedaebu <min.kim163@gmail.com>
2021-01-18 16:03:16 -0500

    finishing up graphnode

--- w4d1_homework/graphnodes_and_bfs_exercise.rb
+++ w4d1_homework/graphnodes_and_bfs_exercise.rb
@@ -3,23 +3,31 @@ class GraphNode
     attr_reader :value
     attr_accessor :neighbors
 
-    def initialize(value, neighbors)
+    def initialize(value)
         @value = value
         @neighbors = []
     end
 
     def bfs(starting_node, target_value)
-        visited = []
+        visited = Set.new
         queue = [starting_node]
 
         until queue.empty?
-            queue.shift
-            
+            if queue[0].value == target_value
+                return queue[0].value
+            end
+            if !visited.include?(queue[0])
+                queue += queue[0].neighbors
+                visited += queue[0]
+            end
+                # remove first element of node
+                # check if queue.shift.value == target
+                # if not, then add to visited array
+                # check on neighbors of the checked node
+                # if checked nodes are not in the visited array, then add to queue array
         end
-
-
+        return nil
     end
-
 end
 
 a = GraphNode.new('a')
@@ -32,3 +40,5 @@ a.neighbors = [b, c, e]
 c.neighbors = [b, d]
 e.neighbors = [a]
 f.neighbors = [e]
+
+p GraphNode.bfs(a, "b")
